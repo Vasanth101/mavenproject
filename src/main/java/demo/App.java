c35403cab4bc00ed9ac031784bb5de830c1a5ddd
Vasanth101 <49906168+Vasanth101@users.noreply.github.com>
2025-09-06 22:55:05 +0530

    Update App.java
    jav1

--- src/main/java/demo/App.java
+++ src/main/java/demo/App.java
@@ -1,13 +1,7 @@
-package demo;
+package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World! from CI CD task package hi from demo by jenkins" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from a bigger Java project 🚀");
     }
 }
